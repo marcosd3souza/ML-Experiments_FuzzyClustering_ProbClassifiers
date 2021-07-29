aaf4f07a9625161dc5aa738d724c2f4d553f9600
Carlos Antônio Alves Júnior <carlosaajunior.jp@gmail.com>
2021-07-29 10:57:03 -0300

    Finalizando a segunda parte do projeto

--- code/classifiers/ensemble.py
+++ code/classifiers/ensemble.py
@@ -1,9 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
-from gaussian_bayesian import BayesGaussian
-from parzen_bayesian import KDEClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from collections import Counter
 
@@ -25,6 +22,15 @@ class Ensemble():
             c = Counter(preds)
             true_predictions.append(c.most_common(1)[0][1])
         return true_predictions
+    
+    def process_class(self, y):
+        temp = y.unique()
+        y = y
+        i = 0
+        for val in temp:
+            y = y.replace(val, i)
+            i += 1
+        return y
 
         
 def get_categorical_columns(x):
@@ -68,78 +74,4 @@ def process_nbinary_columns(x, nbinary):
 
     return x
 
-def process_class(y, bin=False, index=0):
-    temp = y.unique()
-    y = y
-    valoresOriginais = []
-    i = 0
-    for val in temp:
-        if bin:
-            if i == index:
-                y = y.replace(val, 0)
-            else:
-                y = y.replace(val, 1)
-        else:
-            y = y.replace(val, i)
-        valoresOriginais.append((val, i))
-        i += 1
 
-    return y, valoresOriginais
-
-def best_bandwith(X,classe_train, classe_test, base_test):
-    best_band_index = 0
-    best_band_acc = 0
-    band = 0
-    arr = []
-    while ( band <= 1):
-        band += 0.1
-        kde = KDEClassifier(bandwidth=band).fit(X, classe_train)
-        c = kde.predict(base_test)
-        acc_parsen = 0
-        for i in range(400):
-          if (classe_test[i] == c[i]):
-              acc_parsen +=1
-        acc_parsen = acc_parsen/400
-        arr.append(acc_parsen)
-        if (acc_parsen > best_band_acc):
-            best_band_acc = acc_parsen
-            best_band_index = band
-
-    return best_band_index, best_band_acc, arr
-
-path = r"C:\Users\Cejota\Documents\ML\ml-cin-2021\data\input\yeast.data"
-
-df = pd.read_csv(path, delimiter=r"\s+")
-y = df['class']
-x = df.drop(columns=['class', 'sequence_name', 'vac', 'pox', 'erl'])
-
-binary, nbinary = get_categorical_columns(x)
-x = process_binary_columns(x, binary)
-x = process_nbinary_columns(x, nbinary)
-
-y, valoresOriginais = process_class(y)
-
-X_train, X_test, y_train, y_test = train_test_split(x, y,
-                                                    stratify=y,
-                                                    test_size=0.25)
-
-BG = BayesGaussian()
-PG = KDEClassifier()
-LR = LogisticRegression()
-KNN = KNeighborsClassifier()
-LR.fit(X_train, y_train)
-PG.fit(X_train, y_train)
-KNN.fit(X_train, y_train)
-BG.fit(X_train, y_train)
-cls = [PG, BG, LR, KNN]
-ensemble = Ensemble()
-ensemble.predict(classifiers=cls, x=X_test)
-
-#print(np.cov(x).shape)
-#BG.get_bayes_numerator(input_ex=vf, for_class=1)
-#print(BG.get_conditional_prob_gaussian(1, 1, 1))
-#print(BG.predict(X_test))
-#BG.predict(X_test)
-
-
-#LR = LogisticRegression(multi_class="ovr").fit(x, y)
--- code/main.py
+++ code/main.py
@@ -1,3 +1,19 @@
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+from classifiers.gaussian_bayesian import BayesGaussian
+from classifiers.parzen_bayesian import KDEClassifier
+from classifiers.ensemble import Ensemble
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+import csv
+
 from data_reader import DataReader
 from clustering import FuzzyClustering
 import time
@@ -35,3 +51,115 @@ print('J: ', best_J)
 print('prototypes: ', best_prototypes)
 fcm.print_results(y, best_partition, best_U)
 print('################################################################')
+
+
+################################################ Segunda parte do projeto ####################################################
+
+def print_results(y_true, y_predict):
+    f_measure = f1_score(y_true, y_predict, average='macro')
+    conf_matrix = confusion_matrix(y_true, y_predict)
+    acc = accuracy_score(y_true, y_predict)
+    precision = precision_score(y_true, y_predict, average='macro')
+    recall = recall_score(y_true, y_predict, average='macro')
+
+
+
+    print('confusion matrix: ', conf_matrix)
+    print('accuracy: ', acc)
+    print('f-measure: ', f_measure)
+    print('Precision: ', precision)
+    print('Recall: ', recall)
+
+    return acc, f_measure, precision, recall
+
+skf = StratifiedKFold(n_splits=5)
+
+BG = BayesGaussian()
+PG = KDEClassifier()
+LR = LogisticRegression()
+KNN = KNeighborsClassifier()
+cls = [PG, BG, LR, KNN]
+classifiers_names = ['PG', 'BG', 'LR', 'KNN', 'ENSEMBLE']
+ensemble = Ensemble()
+
+path = "data\input\yeast.data"
+
+df = pd.read_csv(path, delimiter=r"\s+")
+
+
+y = df['class']
+X = df.drop(columns=['class', 'sequence_name', 'vac', 'pox', 'erl'])
+y = ensemble.process_class(y)
+
+
+resultados = [[],[],[],[],[]]
+k = 0
+for train_index, test_index in skf.split(X, y):
+    X_train_k, X_test_k = X.iloc[train_index], X.iloc[test_index]
+    y_train_k, y_test_k = y.iloc[train_index], y.iloc[test_index]
+    LR.fit(X_train_k, y_train_k)
+    PG.fit(X_train_k, y_train_k)
+    KNN.fit(X_train_k, y_train_k)
+    BG.fit(X_train_k, y_train_k)
+
+    predict_LR = LR.predict(X_test_k)
+    predict_PG = PG.predict(X_test_k)
+    predict_KNN = KNN.predict(X_test_k)
+    predict_BG = BG.predict(X_test_k)
+
+    predict_ENSEMBLE = ensemble.predict(classifiers=cls, x=X_test_k)
+
+    print("Resultado do classificador LR para K = ", k)
+    acc_LR, f_measure_LR, precision_LR, recall_LR = print_results(y_test_k, predict_LR)
+    resultados[0].append([1 - acc_LR, f_measure_LR, precision_LR, recall_LR])
+
+    print("Resultado do classificador LR para K = ", k)
+    acc_PG, f_measure_PG, precision_PG, recall_PG = print_results(y_test_k, predict_PG)
+    resultados[1].append([1 - acc_PG, f_measure_PG, precision_PG, recall_PG])
+
+    print("Resultado do classificador LR para K = ", k)
+    acc_KNN, f_measure_KNN, precision_KNN, recall_KNN = print_results(y_test_k, predict_KNN)
+    resultados[2].append([1 - acc_KNN, f_measure_KNN, precision_KNN, recall_KNN])
+
+    print("Resultado do classificador LR para K = ", k)
+    acc_BG, f_measure_BG, precision_BG, recall_BG = print_results(y_test_k, predict_BG)
+    resultados[3].append([1 - acc_BG, f_measure_BG, precision_BG, recall_BG])
+
+    print("Resultado do classificador LR para K = ", k)
+    acc_ENSEMBLE, f_measure_ENSEMBLE, precision_ENSEMBLE, recall_ENSEMBLE = print_results(y_test_k, predict_ENSEMBLE)
+    resultados[4].append([1 - acc_ENSEMBLE, f_measure_ENSEMBLE, precision_ENSEMBLE, recall_ENSEMBLE])
+
+    k += 1
+
+n = len(y_test_k)
+k = 0
+
+metricas_friedman = {}
+for name in classifiers_names:
+    res = np.array(resultados[k]).T
+    
+    erro = np.mean(res[0])
+    erro = 1.96 * np.sqrt( (erro * (1 - erro)) / n)
+    print("Erro médio para " + name + " :", (erro * 100), )
+
+    fscore = np.mean(res[1])
+    fscore = 1.96 * np.sqrt( (fscore * (1 - fscore)) / n)
+    print("Fscore médio para " + name + " :", (fscore))
+
+    precision = np.mean(res[2])
+    precision = 1.96 * np.sqrt( (precision * (1 - precision)) / n)
+    print("Precisão média para " + name + " :", (precision * 100))
+
+    cobertura = np.mean(res[3])
+    cobertura = 1.96 * np.sqrt( (cobertura * (1 - cobertura)) / n)
+    print("Recall (cobertura) média para " + name + " :", (cobertura * 100), "\n")
+
+    metricas_friedman[name] = {"Classificador": name, "Erro": erro, "Fscore": fscore, "Precision": precision, "Recall": cobertura}
+    k += 1
+
+with open('data\output\metricas_friedman.csv', 'w') as f:
+    w = csv.DictWriter(f, metricas_friedman['PG'].keys())
+    w.writeheader()
+    for name in classifiers_names:
+        w.writerow(metricas_friedman[name])
+
