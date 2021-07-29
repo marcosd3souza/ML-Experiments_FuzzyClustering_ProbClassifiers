ea7be8f1f1d5614b4fee770ec210be760798c308
marcosd3souza <marcosd3souza@gmail.com>
2021-07-28 23:39:28 -0300

    part 1: added results for fuzzy clustering (FCM-DFCV)

--- code/data_reader.py
+++ code/data_reader.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import parameters as params
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 
 class DataReader:
     def __init__(self):
@@ -7,11 +10,21 @@ class DataReader:
          
         self.data = pd.read_csv(
             path, 
-            header=None, 
             delim_whitespace=True
         )
+
+    def get_y_labels(self, y):
+        temp = np.unique(y)
+        
+        i = 0
+        for val in temp:
+            y[y==val] = i
+            i += 1
+        return np.array(y, dtype=int)
     
     def get_data(self):
-        y = self.data.values[:, -1]
+        y = np.array(self.data.values[:, -1])
+        y = self.get_y_labels(y)
         X = self.data.values[:, 1:-1]
-        return X, y
+        X = self.scaled_data = MinMaxScaler().fit_transform(X)
+        return np.array(X, dtype=float), y
